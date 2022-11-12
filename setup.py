e2d0c151e9dcfcd422a104ff85cba3bef84af9d0
nikitinale <nikitinale@gmail.com>
2022-11-12 08:35:06 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep neural network for gamma-ray spectrum seconvolution',
+    author='Aleksander_Nikitin',
+    license='MIT',
+)
